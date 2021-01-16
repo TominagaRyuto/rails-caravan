34f9fe02216ff600259053d579ad7f7930974fa9
TominagaRyuto <esouunnpou@gmail.com>
2021-01-16 06:26:02 +0000

    [start]3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,10 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
@@ -23,4 +24,5 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
+
 end
